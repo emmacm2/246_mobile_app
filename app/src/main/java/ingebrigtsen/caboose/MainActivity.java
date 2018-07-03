a8412d47cf8e5b2cdfd776bab96a5f512b035f1b
jessicasi <aron.jess@gmail.com>
2018-07-02 19:43:25 -0700

    Initial commit

---
+++ app/src/main/java/ingebrigtsen/caboose/MainActivity.java
@@ -0,0 +1,18 @@
+package ingebrigtsen.caboose;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
