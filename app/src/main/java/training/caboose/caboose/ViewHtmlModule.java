8e1b193da0a2bfc418f49b2957d299da9cce10e5
jessicasi <aron.jess@gmail.com>
2018-07-20 16:28:43 -0700

    Added in basic javadoc details for ViewHTMLModule class

--- app/src/main/java/training/caboose/caboose/ViewHtmlModule.java
+++ app/src/main/java/training/caboose/caboose/ViewHtmlModule.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 
+/**
+ * ViewHTMLModule class adds an activity to display any text modules created
+ * by the employer on the web site
+ */
 public class ViewHtmlModule extends AppCompatActivity {
 
     String htmlData;
@@ -21,11 +25,15 @@ public class ViewHtmlModule extends AppCompatActivity {
         moduleId = intent.getStringExtra(getString(R.string.moduleIntent_ModuleId));
         positionId = intent.getStringExtra("positionId");
 
-        WebView moduleWebView = (WebView) findViewById(R.id.htmlModuleWebView);
+        WebView moduleWebView = /*(WebView)*/ findViewById(R.id.htmlModuleWebView);
         moduleWebView.loadData(htmlData, "text/html", null);
 
     }
 
+    /**
+     * Takes employee to the quiz that goes along with the data they just studied
+     * @param view Current View
+     */
     public void toQuiz (View view) {
         Intent intent = new Intent (this, Quiz.class);
         intent.putExtra(getString(R.string.moduleIntent_ModuleId), moduleId);
